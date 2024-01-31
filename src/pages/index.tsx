71eac5b1199177ae95310f9298eca04d935e77c7
Austin Woon Quan <43132101+austinwoon@users.noreply.github.com>
2024-01-31 23:36:16 +0800

    fix: try to force include pyodide

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,4 +4,16 @@ const LandingPage = () => {
   return <SignIn />
 }
 
+// for some reason standalone builds excludes pyodide, do this so it gets included in standalone https://stackoverflow.com/questions/71422446/nextjs-force-dependency-with-outputstandalone-option
+const requiredStandaloneDependencies = [
+  // some required deps that have not been included in standalone
+  'pyodide',
+]
+
+export const config = {
+  unstable_includeFiles: requiredStandaloneDependencies.map(
+    (dep) => `node_modules/${dep}/**/*.+(js|json)`,
+  ),
+}
+
 export default LandingPage
